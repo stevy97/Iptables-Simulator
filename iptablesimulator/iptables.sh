f5154d355f6c32df055b9bae5ec5f8fa64f2cf47
stevy97 <dubo_s@etna-alternance.net>
2015-11-27 16:08:14 +0100

    Update iptables.sh

--- iptablesimulator/iptables.sh
+++ iptablesimulator/iptables.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+TCP_SERVICES = "22"
+UCP_SERVICES = ""
+REMOTE_TCP_SERVICES ="80 443" # web browsing
+REMOTE_UDP_SERVICES = "53" #DNS
+SSH = "22"
+
+if !(-x /sbin/iptables) #debugging
+exit 0
+fi
+
 /sbin/iptables -F
 /sbin/iptables -X
 
 #Default rules
-/sbin/iptables -P INPUT DROP
-/sbin/iptables -P FORWARD DROP
+/sbin/iptables -P INPUT 
+/sbin/iptables -P FORWARD 
 /sbin/iptables -P OUTPUT ACCEPT
 
 #Accept in/out connexions
-/sbin/iptables -t filter -A INPUT -p TCP --dport 22 -j ACCEPT
-/sbin/iptables -t filter -A OUTPUT -p TCP --sport 22 -j ACCEPT 
+/sbin/iptables -t filter -A INPUT -p TCP --dport ${TCP} -j ACCEPT
+/sbin/iptables -t filter -A OUTPUT -p TCP --sport ${TCP} -j ACCEPT 
 
 #refuse all over connexions
 /sbin/iptables -t filter -A INPUT -j DROP
